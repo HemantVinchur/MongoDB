da18d12ffc4c83fff95de02e5bd8b2367fe03ac2
Hemant Vinchur <hemantvinchur96@gmail.com>
2019-06-14 10:46:47 +0530

    MongoDB update commited

--- connection.js
+++ connection.js
@@ -12,23 +12,32 @@ const dbName = 'myproject';
 const client = new MongoClient(url);
 
 // Use connect method to connect to the Server
-client.connect(function (err, client) {
+client.connect(function(err, client) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
 
   const db = client.db(dbName);
 
-  // Insert a single document
-  db.collection('inserts').insertOne({ a: 1 }, function (err, r) {
-    assert.equal(null, err);
-    assert.equal(1, r.insertedCount);
-
-    // Insert multiple documents
-    db.collection('inserts').insertMany([{ a: 2 }, { a: 3 }], function (err, r) {
-      assert.equal(null, err);
-      assert.equal(2, r.insertedCount);
-      console.log(r);
-      client.close();
-    });
+  // Get the collection
+  const col = db.collection('bulk_write');
+  col.bulkWrite([
+      { insertOne: { document: { a: 1 } } }
+    , { updateOne: { filter: {a:2}, update: {$set: {a:2}}, upsert:true } }
+    , { updateMany: { filter: {a:2}, update: {$set: {a:2}}, upsert:true } }
+    , { deleteOne: { filter: {c:1} } }
+    , { deleteMany: { filter: {c:1} } }
+    , { replaceOne: { filter: {c:3}, replacement: {c:4}, upsert:true}}]
+  , {ordered:true, w:1}, function(err, r) {
+    // assert.equal(null, err);
+    // assert.equal(1, r.insertedCount);
+    // assert.equal(1, Object.keys(r.insertedIds).length);
+    // assert.equal(1, r.matchedCount);
+    // assert.equal(0, r.modifiedCount);
+    // assert.equal(0, r.deletedCount);
+    // assert.equal(2, r.upsertedCount);
+    // assert.equal(2, Object.keys(r.upsertedIds).length);
+    console.log(r);
+    // Ordered bulk operation
+    client.close();
   });
 });
